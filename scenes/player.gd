6903b019ec2822fad00a41fb452721ddd5a2643e
L-Lam <llam5701extra@gmail.com>
2025-03-06 17:59:32 -0800

    other stuff

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,15 +3,15 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print($AnimatedSprite2D.get_children())
-
+	pass
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
-	if Input.is_action_pressed("left"):
+	if Input.is_action_pressed("ui_left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("move")
-	elif Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("ui_right"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("move")
 	else:
