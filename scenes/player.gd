cb92f45d28a722766214c3475d5b1d4dcdcefbd9
L-Lam <llam5701extra@gmail.com>
2025-03-07 19:28:24 -0800

    experimental
    tried some parallaxing, rough idea of what im trying to make

--- scenes/player.gd
+++ scenes/player.gd
@@ -11,8 +11,10 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("move")
+		position.x -= 2
 	elif Input.is_action_pressed("ui_right"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("move")
+		position.x += 2
 	else:
 		$AnimatedSprite2D.play("idle")
