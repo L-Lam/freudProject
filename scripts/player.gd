e348cccec59230cc81c36925f6291100c22ee2c7
L-Lam <llam5701extra@gmail.com>
2025-03-13 19:04:00 -0700

    popup complete

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,20 +1,18 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
+func _process(float) -> void:
 	if Input.is_action_pressed("ui_left"):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("move")
 		position.x -= 2
+		
+		
 	elif Input.is_action_pressed("ui_right"):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("move")
 		position.x += 2
+		
+		
 	else:
 		$AnimatedSprite2D.play("idle")
