b4aa71e06a5a411aebf4d3c66691b9b7050e0313
L-Lam <llam5701extra@gmail.com>
2025-03-18 00:16:03 -0700

    a

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,22 +2,21 @@ extends Node2D
 
 
 func _process(float) -> void:
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left") and -9600 <= position.x:
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("move")
 		position.x -= 7
 		
-		
-	elif Input.is_action_pressed("ui_right"):
+	elif Input.is_action_pressed("ui_right") and position.x <= 11520:
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("move")
 		position.x += 7
 		
-	elif Input.is_action_just_pressed("right_dash"):
+	elif Input.is_action_just_pressed("right_dash") and position.x <= 11520:
 		$AnimatedSprite2D.flip_h = false
 		position.x += 960
 	
-	elif Input.is_action_just_pressed("left_dash"):
+	elif Input.is_action_just_pressed("left_dash") and -9600 <= position.x:
 		$AnimatedSprite2D.flip_h = true
 		position.x -= 960
 
